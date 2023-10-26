08a15b94c0398c42ee96ad010838cd43c6e6fb5a
lsh2200 <121371582+LSH160981@users.noreply.github.com>
2023-10-26 21:20:55 +0800

    "fixed: 对error组件重构"

--- src/components/errormsg.vue
+++ src/components/errormsg.vue
@@ -1,26 +1,37 @@
 <template>
-  <div
-    name="fade"
-    class="max-w-[33.3%] h-16 bg-red-500 bg-opacity-80 rounded-3xl flex justify-start items-center flex-nowrap fixed top-7 left-1/2 -translate-x-1/2 maxd:w-5/6">
-    <img src="@/assets/error.svg" class="w-[45px] h-[45px] mx-3" />
-    <span class="truncate text-xl mr-3">{{ errortitle || "发生错误！" }}</span>
+  <div class="w-5/6 fixed top-7 left-1/2 -translate-x-1/2 flex justify-center text-center">
+    <transition name="fade">
+      <div
+        v-if="show"
+        class="max-w-[100%] w-2/6 h-12 bg-red-500 bg-opacity-80 rounded-3xl 
+        flex justify-center items-center flex-nowrap maxd:w-5/6">
+        <span class="truncate text-xl mx-3">{{ errortitle || "发生错误！" }}</span>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
+
 //子组件接收父组件传递过来的值
 defineProps(["errortitle"]);
+let show = ref(false);
+// 监视传输进来的数据
+onMounted(() => {
+  show.value = true;
+});
 </script>
 <style scoped>
 .fade-enter-from {
   opacity: 0;
-  transform: scale(0);
+  transform: scaleX(0);
 }
 .fade-enter-active {
   transition: all 0.5s;
 }
 .fade-enter-to {
   opacity: 1;
-  transform: scale(1);
+  transform: scaleX(1);
 }
 </style>
