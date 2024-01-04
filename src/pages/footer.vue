2c51011606ed587b682940c71475716fe8cd158d
lsh2200 <121371582+LSH160981@users.noreply.github.com>
2024-01-04 16:47:52 +0800

    "fix: 小变动"

--- src/pages/footer.vue
+++ src/pages/footer.vue
@@ -33,8 +33,6 @@
   <p class="mt-8 text-xs opacity-40 select-none">
     <span class="pr-1">Made by</span>
     <a href="https://github.com/LSH160981/CF_AI" target="_blank"> XiaoLiao </a>
-    <span class="px-1">|</span>
-    <a href="https://github.com/ddiu8081/chatgpt-demo" target="_blank"> Source Code </a>
   </p>
 </template>
 
